299e121e6f5bf31595ec9756ec9bc8640a3b9dbe
Davide Pucci <davidepucci21@gmail.com>
2013-01-11 14:56:08 +0100

    Patched 'services/surfaceflinger/DisplayHardware'

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -34,6 +34,10 @@
 #include "HWComposer.h"
 #include "SurfaceFlinger.h"
 
+#ifdef QCOM_HARDWARE
+#include <qcom_ui.h>
+#endif
+
 namespace android {
 // ---------------------------------------------------------------------------
 
@@ -89,6 +93,7 @@ status_t HWComposer::createWorkList(size_t numLayers) {
             free(mList);
             size_t size = sizeof(hwc_layer_list) + numLayers*sizeof(hwc_layer_t);
             mList = (hwc_layer_list_t*)malloc(size);
+            memset(mList, 0, size);
             mCapacity = numLayers;
         }
         mList->flags = HWC_GEOMETRY_CHANGED;
@@ -98,6 +103,10 @@ status_t HWComposer::createWorkList(size_t numLayers) {
 }
 
 status_t HWComposer::prepare() const {
+#ifdef QCOM_HARDWARE
+    // Reset the Skip composition flag
+    mList->flags &= ~HWC_SKIP_COMPOSITION;
+#endif
     int err = mHwc->prepare(mHwc, mList);
     if (err == NO_ERROR) {
         size_t numOVLayers = 0;
@@ -115,6 +124,12 @@ status_t HWComposer::prepare() const {
                 case HWC_FRAMEBUFFER:
                     numFBLayers++;
                     break;
+#ifdef QCOM_HARDWARE
+                default:
+                    if(isUpdatingFB((HWCCompositionType)l.compositionType))
+                        numFBLayers++;
+                    break;
+#endif
             }
         }
         mNumOVLayers = numOVLayers;
@@ -167,6 +182,12 @@ hwc_layer_t* HWComposer::getLayers() const {
     return mList ? mList->hwLayers : 0;
 }
 
+#ifdef QCOM_HARDWARE
+uint32_t HWComposer::getFlags() const {
+    return mList ? mList->flags : 0;
+}
+#endif
+
 void HWComposer::dump(String8& result, char* buffer, size_t SIZE,
         const Vector< sp<LayerBase> >& visibleLayersSortedByZ) const {
     if (mHwc && mList) {
@@ -191,8 +212,13 @@ void HWComposer::dump(String8& result, char* buffer, size_t SIZE,
             }
             snprintf(buffer, SIZE,
                     " %8s | %08x | %08x | %08x | %02x | %05x | %08x | [%5d,%5d,%5d,%5d] | [%5d,%5d,%5d,%5d] %s\n",
+#ifdef QCOM_HARDWARE
+                    l.compositionType ? (l.compositionType == HWC_OVERLAY ? "OVERLAY" : "COPYBIT") : "FB",
+                    intptr_t(l.handle), l.hints, l.flags, l.transform & FINAL_TRANSFORM_MASK, l.blending, format,
+#else
                     l.compositionType ? "OVERLAY" : "FB",
                     intptr_t(l.handle), l.hints, l.flags, l.transform, l.blending, format,
+#endif
                     l.sourceCrop.left, l.sourceCrop.top, l.sourceCrop.right, l.sourceCrop.bottom,
                     l.displayFrame.left, l.displayFrame.top, l.displayFrame.right, l.displayFrame.bottom,
                     layer->getName().string());
@@ -204,6 +230,14 @@ void HWComposer::dump(String8& result, char* buffer, size_t SIZE,
         result.append(buffer);
     }
 }
+
+#ifdef QCOM_HARDWARE
+void HWComposer::perform(int event, int value) {
+    if (mHwc) {
+        mHwc->perform(mHwc, event, value);
+    }
+}
+#endif
 
 // ---------------------------------------------------------------------------
 }; // namespace android
