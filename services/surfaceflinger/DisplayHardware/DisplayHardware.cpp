d536f8858205fc1186039c450ba18b2e102ecde3
Davide Pucci <davidepucci21@gmail.com>
2013-01-11 14:52:02 +0100

    Patched 'services/surfaceflinger/DisplayHardware'

--- services/surfaceflinger/DisplayHardware/DisplayHardware.cpp
+++ services/surfaceflinger/DisplayHardware/DisplayHardware.cpp
@@ -173,6 +173,17 @@ void DisplayHardware::init(uint32_t dpy)
             LOGW("H/W composition disabled");
             attribs[2] = EGL_CONFIG_CAVEAT;
             attribs[3] = EGL_SLOW_CONFIG;
+#ifdef QCOM_HARDWARE
+        } else {
+            // We have hardware composition enabled. Check the composition type
+            if (property_get("debug.composition.type", property, NULL) > 0) {
+                if ((strncmp(property, "c2d", 3) == 0) ||
+                    (strncmp(property, "dyn", 3) == 0))
+                    mFlags |= C2D_COMPOSITION;
+                else if ((strncmp(property, "mdp", 3)) == 0)
+                    mFlags |= MDP_COMPOSITION;
+            }
+#endif
         }
     }
 
@@ -184,9 +195,13 @@ void DisplayHardware::init(uint32_t dpy)
     eglGetConfigs(display, NULL, 0, &numConfigs);
 
     EGLConfig config = NULL;
+#ifdef FORCE_EGL_CONFIG
+    config = (EGLConfig)FORCE_EGL_CONFIG;
+#else
     err = selectConfigForPixelFormat(display, attribs, format, &config);
     LOGE_IF(err, "couldn't find an EGLConfig matching the screen format");
-    
+#endif
+
     EGLint r,g,b,a;
     eglGetConfigAttrib(display, config, EGL_RED_SIZE,   &r);
     eglGetConfigAttrib(display, config, EGL_GREEN_SIZE, &g);
